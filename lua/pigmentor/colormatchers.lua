4199859a72170cd49b8f2ad42af221425eda2960
Immanuel Haffner <haffner.immanuel@gmail.com>
2025-08-24 23:35:09 +0300

    Fix short RGB and add CSS rgba color matcher

--- lua/pigmentor/colormatchers.lua
+++ lua/pigmentor/colormatchers.lua
@@ -1,8 +1,12 @@
 local hex_digit = '[0-9a-fA-F]'
+local decimal = '%d*%.?%d*'
+
+local function round(n) return math.floor(n + .5) end
 
 -- Array of known color formats.
 local M = {
     {
+        -- Hexadecimal RGBA (8 digits)
         kind = 'hex_rgba',
         pattern = '#' .. string.rep(hex_digit, 8),
         to_vim_color = function(str)
@@ -10,6 +14,7 @@ local M = {
         end,
     },
     {
+        -- Hexadecimal RGB (6 digits)
         kind = 'hex_rgb',
         pattern = '#' .. string.rep(hex_digit, 6),
         to_vim_color = function(str)
@@ -17,13 +22,25 @@ local M = {
         end,
     },
     {
+        -- Short hexadecimal RGB (3 digits)
         kind = 'hex_rgb_short',
         pattern = '#' .. string.rep(hex_digit, 3),
         to_vim_color = function(str)
-            local r = str:sub(2, 2)
-            local g = str:sub(2, 2)
-            local b = str:sub(3, 3)
-            return '#' .. r:rep(2) .. g:rep(2) .. b:rep(2)
+            local R = str:sub(2, 2)
+            local G = str:sub(3, 3)
+            local B = str:sub(4, 4)
+            return '#' .. R:rep(2) .. G:rep(2) .. B:rep(2)
+        end,
+    },
+    {
+        -- CSS rgba
+        kind = 'css_rgba',
+        pattern = 'rgba%(' ..
+                  '%s*(' .. decimal .. ')%s*,%s*(' .. decimal .. ')%s*,%s*(' .. decimal .. ')%s*,%s*(' .. decimal .. ')%s*%)',
+        to_vim_color = function(str)
+            local r, g, b = str:match('rgba%(' .. '%s*(' .. decimal .. ')%s*,%s*(' .. decimal .. ')%s*,%s*(' .. decimal .. ')%s*,%s*(' .. decimal .. ')%s*%)')
+            local R, G, B = round(255 * r), round(255 * g), round(255 * b)
+            return ('#%02x%02x%02x'):format(R, G, B)
         end,
     },
 }
